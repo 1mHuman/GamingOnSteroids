1ba44e40b6a99f7a49427e91cc3e308a7a937f2f
1mHuman <darkveader@gmail.com>
2015-11-04 23:11:56 +0200

    Upgrade to version 1.1
    -JungleClear
    -Killsteal
    -Improved Q logic

--- Vi.lua
+++ Vi.lua
@@ -1,93 +1,102 @@
-if GetObjectName(myHero) ~= "Vi" then return end
+if GetObjectName(GetMyHero()) ~= "Vi" then return end
 
-ViMenu = Menu("Vi", "Vi")
-ViMenu:SubMenu("Combo", "Combo")
+if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua - Go download it and save it Common!") return end
+
+local ViMenu = MenuConfig("Vi", "Vi")
+ViMenu:Menu("Combo", "Combo")
 ViMenu.Combo:Boolean("Q", "Use Q", true)
 ViMenu.Combo:Boolean("E", "Use E", true)
 ViMenu.Combo:Boolean("R", "Use R", true)
 ViMenu.Combo:Slider("Rhp", "Use R if Target Health % <", 30, 1, 100, 1)
 
-ViMenu:SubMenu("Harass", "Harass")
+ViMenu:Menu("Harass", "Harass")
 ViMenu.Harass:Boolean("Q", "Use Q", true)
 ViMenu.Harass:Boolean("E", "Use E", true)
 
-ViMenu:SubMenu("JungleSteal", "Jungle Steal")
-ViMenu.JungleSteal:Boolean("Baron", "Auto smite Baron", true)
-ViMenu.JungleSteal:Boolean("Dragon", "Auto smite Dragon", true)
+ViMenu:Menu("Killsteal", "Killsteal")
+ViMenu.Killsteal:Boolean("R", "Killsteal with R", true)
 
-ViMenu:SubMenu("Misc", "Misc")
-ViMenu.Misc:Boolean("Interrupt", "Interrupt with Q", true)
+ViMenu:Menu("JungleClear", "Jungle Clear")
+ViMenu.JungleClear:Boolean("Q", "Use Q", true)
+ViMenu.JungleClear:Boolean("E", "Use E", false)
+ViMenu.JungleClear:Slider("Mana", "if Mana % >", 30, 0, 80, 1)
 
-ViMenu:SubMenu("Drawings", "Drawings")
+ViMenu:Menu("Drawings", "Drawings")
 ViMenu.Drawings:Boolean("R", "Draw R Range", true)
 
-ViMenu:Info("info0", " ")
-ViMenu:Info("info1", "Vi the Heartbreaker")
-ViMenu:Info("info2", "By ImHuman - v1.0")
+local InterruptMenu = MenuConfig("Interrupt (Q)", "Interrupt")
 
 CHANELLING_SPELLS = {
-    ["Caitlyn"]                     = {_R},
-    ["Katarina"]                    = {_R},
-    ["FiddleSticks"]                = {_R},
-    ["Galio"]                       = {_R},
-    ["Lucian"]                      = {_R},
-    ["MissFortune"]                 = {_R},
-    ["VelKoz"]                      = {_R},
-    ["Nunu"]                        = {_R},
-    ["Karthus"]                     = {_R},
-    ["Malzahar"]                    = {_R},
-    ["Xerath"]                      = {_R},
+    ["CaitlynAceintheHole"]         = {Name = "Caitlyn",      Spellslot = _R},
+    ["Drain"]                       = {Name = "FiddleSticks", Spellslot = _W},
+    ["Crowstorm"]                   = {Name = "FiddleSticks", Spellslot = _R},
+    ["GalioIdolOfDurand"]           = {Name = "Galio",        Spellslot = _R},
+    ["FallenOne"]                   = {Name = "Karthus",      Spellslot = _R},
+    ["KatarinaR"]                   = {Name = "Katarina",     Spellslot = _R},
+    ["LucianR"]                     = {Name = "Lucian",       Spellslot = _R},
+    ["AlZaharNetherGrasp"]          = {Name = "Malzahar",     Spellslot = _R},
+    ["MissFortuneBulletTime"]       = {Name = "MissFortune",  Spellslot = _R},
+    ["AbsoluteZero"]                = {Name = "Nunu",         Spellslot = _R},
+    ["Pantheon_GrandSkyfall_Jump"]  = {Name = "Pantheon",     Spellslot = _R},
+    ["ShenStandUnited"]             = {Name = "Shen",         Spellslot = _R},
+    ["UrgotSwap2"]                  = {Name = "Urgot",        Spellslot = _R},
+    ["VarusQ"]                      = {Name = "Varus",        Spellslot = _Q},
+    ["InfiniteDuress"]              = {Name = "Warwick",      Spellslot = _R}
 }
 
+DelayAction(function()
+  local str = {[_Q] = "Q", [_W] = "W", [_E] = "E", [_R] = "R"}
+  for i, spell in pairs(CHANELLING_SPELLS) do
+    for _,k in pairs(GetEnemyHeroes()) do
+        if spell["Name"] == GetObjectName(k) then
+        InterruptMenu:Boolean(GetObjectName(k).."Inter", "On "..GetObjectName(k).." "..(type(spell.Spellslot) == 'number' and str[spell.Spellslot]), true)
+        end
+    end
+  end
+end, 1)
 
-local callback = nil
+local isCastingQ = false
+local qTime = 0
+local qRangeMin = GetCastRange(myHero,_Q)
+local qRangeMax = 800
 
-OnProcessSpell(function(unit, spell)
-        if not callback or not unit or GetObjectType(unit) ~= Obj_AI_Hero  or GetTeam(unit) == GetTeam(GetMyHero()) then return end
-        local unitChanellingSpells = CHANELLING_SPELLS[GetObjectName(unit)]
-
-        if unitChanellingSpells then
-            for _, spellSlot in pairs(unitChanellingSpells) do
-                if spell.name == GetCastName(unit, spellSlot) then callback(unit, CHANELLING_SPELLS) end
-            end
-	end
+OnDraw(function(myHero)
+if ViMenu.Drawings.R:Value() then DrawCircle(myHeroPos(),GetCastRange(myHero,_R),3,100,0xff00ff00) end
 end)
 
-function addInterrupterCallback( callback0 )
-callback = callback0
-end
-
-OnLoop(function(myHero)
-
-if IOW:Mode() == "Combo" then
+OnTick(function(myHero)
 
 local target = GetCurrentTarget()
-local myHeroPos = GoS:myHeroPos()
+local myHeroPos = myHeroPos()
 
-
-if CanUseSpell(myHero,_Q) == READY and GoS:ValidTarget(target, 800) and ViMenu.Combo.Q:Value() then
-      CastSkillShot(_Q, myHeroPos.x, myHeroPos.y, myHeroPos.z)
-      for i=250, 750, 125 do
-        GoS:DelayAction(function()
-              local _Qrange = 250 + math.min(250, i/2)
-              local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),math.huge,600,_Qrange,100,false,true)
-              if QPred.HitChance == 1 then
-                CastSkillShot2(_Q, QPred.PredPos.x, QPred.PredPos.y, QPred.PredPos.z)
-              end
-          end, i)
-      end
+if IOW:Mode() == "Combo" then
+if CanUseSpell(myHero,_Q) == READY and ValidTarget(target, 800) and ViMenu.Combo.Q:Value() then
+	if not target then return end
+	if IsInDistance(target, qRangeMax) and CanUseSpell(myHero,_Q) == READY then
+		local qRange = qRangeMin + (GetGameTimer() - qTime) * 500
+		if qRange > qRangeMax then	qRange = qRangeMax end
+		if not isCastingQ then
+      CastSkillShot(_Q,GetMousePos())
+      return
+    end
+    if IsInDistance(target, qRange) then
+    	local pred = GetPredictionForPlayer(GetOrigin(myHero),target,GetMoveSpeed(target),math.huge,600,qRange,100,false,true)
+    	if pred.HitChance == 1 and qRange > 700 then
+    		CastSkillShot2(_Q,pred.PredPos)
+    	end
+		end
+	end
     end
 
 
-if CanUseSpell(myHero,_E) == READY and GoS:ValidTarget(target, 175) and ViMenu.Combo.E:Value() then
+if CanUseSpell(myHero,_E) == READY and ValidTarget(target, 175) and ViMenu.Combo.E:Value() then
     CastSpell(_E)
     end
 
-
 if ViMenu.Combo.R:Value() then
-    for _, enemy in pairs(Gos:GetEnemyHeroes()) do
-	   if CanUseSpell(myHero,_R) == READY and 100*GetCurrentHP(enemy)/GetMaxHP(enemy) <= ViMenu.Combo.Rhp:Value() and GoS:ValidTarget(target, 800) then
-	CastTargetSpell(target, _R)
+    for _, enemy in pairs(GetEnemyHeroes()) do
+	   if CanUseSpell(myHero,_R) == READY and 100*GetCurrentHP(enemy)/GetMaxHP(enemy) <= ViMenu.Combo.Rhp:Value() and ValidTarget(enemy, 800) then
+	CastTargetSpell(enemy, _R)
     end
   end
 end
@@ -95,60 +104,97 @@ end
 end
 
 if IOW:Mode() == "Harass"  then
-
-local target = GetCurrentTarget()
-local myHeroPos = GoS:myHeroPos()
-
-if CanUseSpell(myHero,_Q) == READY and GoS:ValidTarget(target, 800) and ViMenu.Harass.Q:Value() then
-      CastSkillShot(_Q, myHeroPos.x, myHeroPos.y, myHeroPos.z)
-      for i=250, 750, 125 do
-        GoS:DelayAction(function()
-              local _Qrange = 250 + math.min(250, i/2)
-              local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),math.huge,600,_Qrange,100,false,true)
-              if QPred.HitChance == 1 then
-                CastSkillShot2(_Q, QPred.PredPos.x, QPred.PredPos.y, QPred.PredPos.z)
-              end
-          end, i)
-      end
+if CanUseSpell(myHero,_Q) == READY and ValidTarget(target, 800) and ViMenu.Harass.Q:Value() then
+	  if not target then return end
+	if IsInDistance(target, qRangeMax) and CanUseSpell(myHero,_Q) == READY then
+		local qRange = qRangeMin + (GetGameTimer() - qTime) * 500
+		if qRange > qRangeMax then	qRange = qRangeMax end
+		if not isCastingQ then
+      CastSkillShot(_Q,GetMousePos())
+      return
+    end
+    if IsInDistance(target, qRange) then
+    	local pred = GetPredictionForPlayer(GetOrigin(myHero),target,GetMoveSpeed(target),math.huge,600,qRange,100,false,true)
+    	if pred.HitChance == 1 then
+    		CastSkillShot2(_Q,pred.PredPos)
+    	end
+		end
+	end
     end
 
-
-if CanUseSpell(myHero,_E) == READY and GoS:ValidTarget(target, 175) and ViMenu.Harass.E:Value() then
+if CanUseSpell(myHero,_E) == READY and ValidTarget(target, 175) and ViMenu.Harass.E:Value() then
     CastSpell(_E)
     end
-
-
 end
 
-local smitetable = {390, 410, 430, 450, 480, 510, 540, 570, 600, 640, 680, 720, 760, 800, 850, 900, 950, 1000}
-local smitedamage = smitetable[GetLevel(myHero)]
-
-for _,mob in pairs(GoS:GetAllMinions(MINION_JUNGLE)) do
-    if GoS:IsInDistance(mob, 500) then
-	  if CanUseSpell(myHero, Smite) == READY and ViMenu.JungleSteal.Baron:Value() and GetObjectName(mob) == "SRU_Baron" and GetCurrentHP(mob) < smitedamage then
-	  CastTargetSpell(mob, Smite)
-	  elseif CanUseSpell(myHero, Smite) == READY and ViMenu.JungleSteal.Dragon:Value() and GetObjectName(mob) == "SRU_Dragon" and GetCurrentHP(mob) < smitedamage then
-	  CastTargetSpell(mob, Smite)
-	  end
+	if ViMenu.Killsteal.R:Value() then
+    for _, enemy in pairs(GetEnemyHeroes()) do
+	   if CanUseSpell(myHero,_R) == READY and GetCurrentHP(enemy)+GetDmgShield(enemy) < CalcDamage(myHero, enemy, 150*GetCastLevel(myHero,_R) + 1.4*GetBonusDmg(myHero)) and ValidTarget(enemy, 800) then
+	CastTargetSpell(enemy, _R)
     end
+  end
 end
 
+ for i,mobs in pairs(minionManager.objects) do
+        if IOW:Mode() == "LaneClear" and GetTeam(mobs) == 300 and GetPercentMP(myHero) >= ViMenu.JungleClear.Mana:Value() then
+          if CanUseSpell(myHero,_Q) == READY and ViMenu.JungleClear.Q:Value() and ValidTarget(mobs, 800) then
+          if not mobs then return end
+	     if IsInDistance(mobs, qRangeMax) and CanUseSpell(myHero,_Q) == READY then
+		local qRange = qRangeMin + (GetGameTimer() - qTime) * 500
+		if qRange > qRangeMax then	qRange = qRangeMax end
+		if not isCastingQ then
+      CastSkillShot(_Q,GetMousePos())
+      return
+    end
+    if IsInDistance(mobs, qRange) then
+    		CastSkillShot2(_Q,GetOrigin(mobs))
+		end
+	  end
+	end
 
-if ViMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
+	  if CanUseSpell(myHero,_E) == READY and ViMenu.JungleClear.E:Value() and ValidTarget(mobs, 175) then
+	  CastSpell(_E)
+          end
+        end
+    end
 end)
 
-
-addInterrupterCallback(function(target, spellType)
-  if CanUseSpell(myHero,_Q) == READY and GoS:ValidTarget(target, 800) and ViMenu.Misc.Interrupt:Value() and spellType == CHANELLING_SPELLS then
-  CastSkillShot(_Q, myHeroPos.x, myHeroPos.y, myHeroPos.z)
-      for i=250, 750, 125 do
-        GoS:DelayAction(function()
-              local _Qrange = 250 + math.min(250, i/2)
-              local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),math.huge,600,_Qrange,100,false,true)
-              if QPred.HitChance == 1 then
-                CastSkillShot2(_Q, QPred.PredPos.x, QPred.PredPos.y, QPred.PredPos.z)
-              end
-          end, i)
+OnProcessSpell(function(unit, spell)
+    if GetObjectType(unit) == Obj_AI_Hero and GetTeam(unit) ~= GetTeam(myHero) and CanUseSpell(myHero,_Q) == READY then
+      if CHANELLING_SPELLS[spell.name] then
+        if ValidTarget(unit, 800) and GetObjectName(unit) == CHANELLING_SPELLS[spell.name].Name and InterruptMenu[GetObjectName(unit).."Inter"]:Value() then
+        local qRange = qRangeMin + (GetGameTimer() - qTime) * 500
+		if qRange > qRangeMax then	qRange = qRangeMax end
+		if not isCastingQ then
+      CastSkillShot(_Q,GetMousePos())
+    end
+	if IsInDistance(unit, qRange) then
+    	local pred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit),math.huge,600,qRange,100,false,true)
+    	if pred.HitChance == 1 then
+    		CastSkillShot2(_Q,pred.PredPos)
+    	end
+		end
+        end
       end
-  end
+    end
+end)
+
+local qBuffName = "ViQ"
+OnUpdateBuff(function(object,buffProc)
+if object == myHero then
+		local buffName = buffProc.Name
+		if buffName == qBuffName then
+			isCastingQ = true
+			qTime = buffProc.StartTime
+		end
+	end
+end)
+
+OnRemoveBuff(function(object,buffProc)
+	if object == myHero then
+		local buffName = buffProc.Name
+		if buffName == qBuffName then
+			isCastingQ = false
+		end
+	end
 end)
